ecc1538646d6d2e12c343bd6b26f7b3023444c7a
root <root@admn.localdomain>
2018-09-19 19:25:01 -0400

    Finished adding some initial routes

--- backend/route.py
+++ backend/route.py
@@ -1,16 +1,37 @@
 from flask import Flask
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+import json
+from flask import Response
 
 app = Flask(__name__)
+FlaskJSON(app)
 
 @app.route('/')
 def home():
     return 'HOME'
 
-@app.route('/api/login')
+@app.route('/api/login/')
 def login():
-    return json_response('LOGIN')
+    data = {
+        'LOGIN'  : '1',
+        'message' : 'SUCCESS',
+        'prefences' : 'New York, Chicago, Boston`'
 
-@app.route('/api/signup')
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/signup/')
 def signup():
-    return 'SIGNUP'
+    data = {
+        'SIGNUP'  : '1',
+        'message' : 'SUCCESS',
+        'prefences' : 'New York, Chicago, Boston`'
+
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
